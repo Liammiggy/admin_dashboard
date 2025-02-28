b95d48815b71a3090612ca12f99cfb5d4d399367
Administrator <migoystudio@gmail.com>
2025-02-28 18:43:07 +0800

    Set login screen as the initial page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
+// import 'package:flutter/material.dart';
+// import 'screens/login_screen.dart'; // Import the Login Page
+
+// void main() {
+//   runApp(const AdminDashboardApp());
+// }
+
+// class AdminDashboardApp extends StatelessWidget {
+//   const AdminDashboardApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Admin Dashboard',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: LoginScreen(), // Set Login Screen as First Page
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart'; // Import the Login Page
+import 'screens/login_screen.dart';
 
 void main() {
-  runApp(const AdminDashboardApp());
+  runApp(MyApp());
 }
 
-class AdminDashboardApp extends StatelessWidget {
-  const AdminDashboardApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Admin Dashboard',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginScreen(), // Set Login Screen as First Page
+      home: LoginScreen(), // Set login_screen.dart as the first screen
     );
   }
 }
